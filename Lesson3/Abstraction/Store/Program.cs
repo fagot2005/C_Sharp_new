1fd505c43bd77a499e1471b77102b4c1d3e744ff
fagot2005@gmail.com <v.lashchenko.1976@gmail.com>
2021-04-02 23:18:54 +0300

    Store -2

--- Lesson3/Abstraction/Store/Program.cs
+++ Lesson3/Abstraction/Store/Program.cs
@@ -23,23 +23,45 @@ namespace Store
         static void Main(string[] args)
         {
             Store store = new Store();
-            Console.WriteLine("Hello World!");
-            //Article[] array = new Article[4];
-            //Article milk = new Article("Milk", "ATB", 28.7);
-            //array[0] = milk;
+            Console.WriteLine("Enter article index");
+            int index = Convert.ToInt32(Console.ReadLine());
+            if (index <= 3)
+                Console.WriteLine("ArticleName {0}, Store {1}, Price {2}", store.Article[index].ArticleName, store.Article[index].StoreName, store.Article[index].Price);
+            Console.WriteLine("Enter article name");
+            string name = Console.ReadLine();
+            int countarticle = 0;
+            for (int i = 0; i < store.Article.Length; i++)
+            {
+                if (String.Equals(store.Article[i].ArticleName, name))
+                {
+                    Console.WriteLine("ArticleName {0}, Store {1}, Price {2}", store.Article[i].ArticleName, store.Article[i].StoreName, store.Article[i].Price);
+                    countarticle++;
+                    break;
+                }
+            }
+            if (countarticle == 0)
+                Console.WriteLine("Article with name {0} not found.", name);
+            Console.ReadKey();
         }
     }
 
     class Store
     {
-        public Article[] array1 = new Article[4];
-        Article milk1 = new Article("Milk", "ATB", 28.7);
-        array1[0] = milk1;
-        
-        //Article cola = new Article("Coca-Cola", "Novus", 19.99);
-        //Article bread = new Article("Borodinsky", "Cilpo", 12.80);
-        //Article orange = new Article("Orange", "ATB", 32.50);
-        
+        public Article[] Article { get; set; }
+
+        public Store()
+        {
+            Article[] array = new Article[4];
+            Article milk = new Article("Milk", "ATB", 28.7);
+            Article cola = new Article("Coca-Cola", "Novus", 19.99);
+            Article bread = new Article("Borodinsky", "Cilpo", 12.80);
+            Article orange = new Article("Orange", "ATB", 32.50);
+            array[0] = milk;
+            array[1] = cola;
+            array[2] = bread;
+            array[3] = orange;
+            this.Article = array;
+        }
     }
 
     class Article
