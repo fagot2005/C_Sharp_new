dc0504f91b2611f5a53fe114d5c44dbaea5c6018
fagot2005@gmail.com <v.lashchenko.1976@gmail.com>
2021-04-05 17:15:55 +0300

    Static class

---
+++ Lesson3/Abstraction/Static/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount = new BankAccount(123, 2000, 0);
+            bankAccount.FinalSumm = bankAccount.Summ;
+            Console.WriteLine("FinalSumm = {0} USD.", bankAccount.FinalSumm);
+            Console.WriteLine("FinalSumm = {0} USD.", bankAccount.FinalSumm + Rate.BalanceAtTheBeginningOfTheDay(bankAccount));
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class BankAccount
+    {
+        public int ID { get; set; }
+        public double Summ { get; set; }
+        public double FinalSumm { get; set; }
+
+        public BankAccount(int iD, double summ, double finalSumm)
+        {
+            ID = iD;
+            Summ = summ;
+            FinalSumm = finalSumm;
+        }
+    }
+
+    static class Rate
+    {
+        private static double rate = 0.22;
+
+        public static double BalanceAtTheBeginningOfTheDay(BankAccount bankAccount)
+        {
+            return bankAccount.FinalSumm * Rate.rate;
+        }
+    }
+}
---
+++ Lesson3/Abstraction/Structure/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Structure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStrust myStrust = new MyStrust();
+            myStrust.Summ = 55;
+
+            Console.WriteLine(myStrust.Summ);
+        }
+    }
+
+    struct MyStrust
+    {
+        public int Summ { get; set; }
+    }
+
+
+}
