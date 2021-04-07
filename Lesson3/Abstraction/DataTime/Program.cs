c01d9e2f02697373c39fe2f1268d285ee14211c2
fagot2005@gmail.com <v.lashchenko.1976@gmail.com>
2021-04-07 21:15:15 +0300

    Print

---
+++ Lesson3/Abstraction/DataTime/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DataTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataTime1 = new DateTime(2021, 01, 01);
+            DateTime dataTime2 = DateTime.Now.Date;
+            Console.WriteLine(dataTime1.Date);
+            Console.WriteLine(dataTime2.Date);
+            TimeSpan res = dataTime2.Date - dataTime1.Date;
+            int countDays = res.Days;
+            Console.WriteLine(res.Days);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson3/Abstraction/Print/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+//using static System.Net.Mime.MediaTypeNames;
+
+namespace Print
+{
+
+    //    Используя Visual Studio, создайте проект по шаблону Console Application.
+    //Создайте статический класс с методом void Print (string stroka, int color), 
+    //        который выводит на экран строку заданным цветом.Используя перечисление, 
+    //        создайте набор цветов, доступных пользователю. Ввод строки и выбор цвета предоставьте пользователю.
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool boll = true;
+            while (boll)
+            {
+                Console.Clear();
+                Console.WriteLine("ВВЕДИ СЛОВО");
+                string str = Console.ReadLine();
+                Console.WriteLine("ВЫБЕРИ ЦВЕТ: СИНИЙ = 9, ЗЕЛЕНЫЙ = 10, ГОЛУБОЙ = 11, КРАСНЫЙ = 12, ПУРПУРНЫЙ = 13, ЖЕЛТЫЙ = 14");
+                int color = Convert.ToInt32(Console.ReadLine());
+                Print.PrintM(str, color);
+                Console.WriteLine("ПРОДОЛЖИТЬ - 1, НЕТ - 2");
+                string answer = Console.ReadLine();
+                if (String.Equals(answer, "1"))
+                    boll = true;
+                else boll = false;
+            }
+        }
+
+        static class Print
+        {
+            public static void PrintM(string str, int color)
+            {
+                ConsoleColor col = new ConsoleColor();
+
+                switch (color)
+                {
+                    case 9:
+                        col = ConsoleColor.Blue;
+                        break;
+                    case 10:
+                        col = ConsoleColor.Green;
+                        break;
+                    case 11:
+                        col = ConsoleColor.Cyan;
+                        break;
+                    case 12:
+                        col = ConsoleColor.Red;
+                        break;
+                    case 13:
+                        col = ConsoleColor.Magenta;
+                        break;
+                    case 14:
+                        col = ConsoleColor.Yellow;
+                        break;
+
+                    default:
+                        col = ConsoleColor.Black;
+                        break;
+                }
+                Console.ForegroundColor = col;
+                Console.WriteLine("{0}", str);
+                Console.ResetColor();
+            }
+        }
+    }
+}
