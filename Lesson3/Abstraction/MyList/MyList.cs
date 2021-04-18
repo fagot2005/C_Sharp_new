a692b8ab1073e59d55724b49a074ce7bd5c0567a
fagot2005@gmail.com <v.lashchenko.1976@gmail.com>
2021-04-18 22:13:56 +0300

    MyList

--- Lesson3/Abstraction/MyList/MyList.cs
+++ Lesson3/Abstraction/MyList/MyList.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,65 @@ namespace MyList
 {
     class MyList<T>
     {
-        Element<T>[] elements = new Element [10];
+        T[] array = null;
+
+        public MyList()
+        {
+            array = new T[5];
+        }
+
+        public void Add(T elem)
+        {
+            if (poss == array.Length - 1)
+            {
+
+                T[] arr = new T[array.Length * 2];
+                array.CopyTo(arr, 0);
+                array = arr;
+                poss++;
+                array[poss] = elem;
+
+            }
+            else
+            {
+                poss++;
+                array[poss] = elem;
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+        }
+
+        int poss = -1;
+
+        public void Reset()
+        {
+            poss = -1;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            poss = -1;
+            while (true)
+            {
+                if (poss < array.Length - 1)
+                {
+                    poss++;
+                    yield return array[poss];
+                }
+                else
+                {
+                    Reset();
+                    yield break;
+                }
+            }
+
+
+        }
     }
 }
--- Lesson3/Abstraction/MyList/Program.cs
+++ Lesson3/Abstraction/MyList/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyList
 {
+    //    Используя Visual Studio, создайте проект по шаблону Console Application.
+    //Создайте коллекцию MyList<T>. Реализуйте в простейшем приближении возможность
+    //использования ее экземпляра аналогично экземпляру класса List<T>. Минимально
+    //требуемый интерфейс взаимодействия с экземпляром, должен включать метод добавления
+    //элемента, индексатор для получения значения элемента по указанному индексу и свойство
+    //только для чтения для получения общего количества элементов. Реализуйте возможность
+    //перебора элементов коллекции в цикле foreach. 
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            MyList<int> list = new MyList<int>();
+            for (int i = 0; i < 50; i++)
+            {
+                list.Add(i+1);
+            }
+            foreach (var element in list)
+            {
+                Console.WriteLine(element);
+            }
+            Console.ReadKey();
         }
     }
 }
