a8b1c8295298b8eb7fe7c58292b0331632c4c18b
fagot2005@gmail.com <v.lashchenko.1976@gmail.com>
2021-06-08 22:38:20 +0300

    Equals_HashCode

---
+++ Lesson3/Abstraction/Equals_HashCode/Block.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equals_HashCode
+{
+    class Block
+    {
+        int line1, line2, line3, line4;
+
+        public Block(int line1, int line2, int line3, int line4)
+        {
+            this.line1 = line1;
+            this.line2 = line2;
+            this.line3 = line3;
+            this.line4 = line4;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || this.GetType() != obj.GetType())
+                return false;
+
+            Block p = (Block)obj;
+            return (line1 == p.line1) && (line2 == p.line2) && (line3 == p.line3) && (line4 == p.line4);
+        }
+
+        public override int GetHashCode()
+        {
+            return line1^line2^line3^line4;
+        }
+
+        public override string ToString()
+        {
+            return "Информация о полях блока " + line1 + ", " + line2 + ", " + line3 + ", " + line4;
+        }
+    }
+}
---
+++ Lesson3/Abstraction/Equals_HashCode/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Equals_HashCode
+{
+    class Program
+    {
+        //        Создайте класс Block с 4-мя полями сторон, переопределите в нем методы:  
+        //        Equals – способный сравнивать блоки между собой,  
+        //        ToString – возвращающий информацию о полях блока.
+
+
+        static void Main(string[] args)
+        {
+            Block one = new Block(12, 15, 4, 9);
+            Block two = new Block(4, 9, 12, 15);
+
+            Console.WriteLine(one.ToString());
+            Console.WriteLine(two.ToString());
+
+            Console.WriteLine(new string(' ', 30));
+
+            Console.WriteLine("Блок 1 равен блоку 2 = {0}", one.Equals(two));
+            if (one.GetHashCode() == two.GetHashCode())
+                Console.WriteLine("Периметры блоков равны между собой");
+            else
+                Console.WriteLine("Периметры блоков не равны между собой");
+
+
+            Console.ReadKey();
+        }
+    }
+}
